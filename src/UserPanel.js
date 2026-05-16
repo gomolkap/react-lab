e25b2b3c0c78f2a9ec2754f30f444207c1c3e4c5
gomolkap <pgomolka@student.agh.edu.pl>
2026-05-16 15:07:22 +0200

    końcówka

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,9 +1,11 @@
 import './App.css'
 import {useState} from "react";
+import MeetingsPage from "./meetings/MeetingsPage";
 
 export default function UserPanel(props) {
     return <div>
         <h4>Witaj {props.email}</h4>
+        <MeetingsPage/>
         <a class="button button-clear" onClick={() => props.onLogout()}>Wyloguj</a>
     </div>
 }
