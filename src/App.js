8d7763f3ddc36c0bf1e4e1196410db2d3ff24004
gomolkap <pgomolka@student.agh.edu.pl>
2026-05-16 13:04:31 +0200

    Dodanie frameworku

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import "milligram";
 
 function App() {
-    const [email, setEmail] = useState('pgomolka@agh.edu.pl');
+    const [email, setEmail] = useState('');
     const [isLoggedIn, setIsLoggedIN] = useState(false);
 
     return (
-        <div>
+        <div className="container">
             <h1>Witaj w systemie zapisów na zajęcia</h1>
             {!isLoggedIn && <div>
+                <h4>Zaloguj się emailem</h4>
             <input type="text" onChange={(event)=>setEmail(event.target.value)}/>
             <button onClick={() => setIsLoggedIN(true)}>Zaloguj się </button>
             </div>}
-                {isLoggedIn && <div>
-                    <h1>Witaj {email}</h1>
-                    <a onClick={()=>setIsLoggedIN(false)}>Wyloguj</a>
-                </div>}
+            {isLoggedIn && <div>
+                    <h4>Witaj {email}</h4>
+                    <a class="button button-clear" onClick={()=>setIsLoggedIN(false)}>Wyloguj</a>
+            </div>}
         </div>
     );
 }
