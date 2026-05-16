2f287e65056fe257a180fc5233d5b2fa5e7737e3
gomolkap <pgomolka@student.agh.edu.pl>
2026-05-16 11:26:03 +0200

    Zmiany walidacyjne

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'pgomolka@agh.edu.pl';
+    const [email, setEmail] = useState('pgomolka@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let validationMessage = '';
+    if (email.length<10){
+        validationMessage=<div>Co za krótki email XD</div>
+    } else if (email.length<15){
+        validationMessage=<div>Ale masz spoko email</div>
+    } else {
+        validationMessage=<div>Ale bydle, takim to można zabić</div>
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <input type="text" value={email} onChange={handleChange}/>
+            { email.length>0 && <div>{validationMessage}</div> }
         </div>
     );
 }
